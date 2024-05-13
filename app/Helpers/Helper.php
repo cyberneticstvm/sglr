795bc4e236c6ac5e89bc5f311b27cd910b1d0880
Cybernetics <mail@cybernetics.me>
2024-05-13 12:09:24 +0530

    small updates

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -2,6 +2,7 @@
 
 use App\Models\Survey;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 function roles()
 {
@@ -15,5 +16,9 @@ function statuses()
 
 function submittedAssessmentCount($district)
 {
-    return Survey::whereIn('created_by', User::where('district_id', $district)->pluck('id'))->count('id');
+    return Survey::when(in_array(Auth::user()->role, ['Administrator', 'Staff', 'Approver']), function ($q) use ($district) {
+        return $q->whereIn('created_by', User::where('district_id', $district)->pluck('id'));
+    })->when(in_array(Auth::user()->role, ['Public']), function ($q) {
+        return $q->where('created_by', Auth::user()->id);
+    })->count('id');
 }
