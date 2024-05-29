d5b855c4e6ff37743a17883c9d1f2e73b511b3d4
Cybernetics <mail@cybernetics.me>
2024-05-29 08:47:18 +0530

    small updates

--- public/assets/js/data-table.js
+++ public/assets/js/data-table.js
@@ -1,6 +1,6 @@
-(function($) {
+(function ($) {
   'use strict';
-  $(function() {
+  $(function () {
     $('#order-listing').DataTable({
       "aLengthMenu": [
         [5, 10, 15, -1],
@@ -9,9 +9,14 @@
       "iDisplayLength": 10,
       "language": {
         search: ""
+      },
+      "layout": {
+        topStart: {
+          buttons: ['pdf']
+        }
       }
     });
-    $('#order-listing').each(function() {
+    $('#order-listing').each(function () {
       var datatable = $(this);
       // SEARCH - Add the placeholder for Search and Turn this into in-line form control
       var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
--- resources/views/web/base.blade.php
+++ resources/views/web/base.blade.php
@@ -69,13 +69,25 @@
     <script src="{{ asset('/assets/js/template.js') }}"></script>
     <script src="{{ asset('/assets/js/settings.js') }}"></script>
     <script src="{{ asset('/assets/js/todolist.js') }}"></script>
-    <script src="{{ asset('/assets/vendors/datatables.net/jquery.dataTables.js') }}"></script>
-    <script src="{{ asset('/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+    <!--<script src="{{ asset('/assets/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>-->
     <!-- endinject -->
     <!-- Custom js for this page-->
     <script src="{{ asset('/assets/js/dashboard.js') }}"></script>
     <script src="{{ asset('/assets/js/Chart.roundedBarCharts.js') }}"></script>
+
+
+    <script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.bootstrap5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.print.min.js"></script>
+
     <script src="{{ asset('/assets/js/data-table.js') }}"></script>
+
     <script src="{{ asset('/assets/js/script.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script>
